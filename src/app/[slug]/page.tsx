f4aa74cc2160704d4146363d5080e64067ae65a3
Matheus Olivieri <matheus.olivieri@yahoo.com>
2025-05-13 21:03:25 -0300

    change files structure to add slug param

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -3,14 +3,19 @@ import styles from "./page.module.css";
 import { TransparentLinkButton } from "@/components/transparent-link-button";
 import { BottomBlur } from "@/components/bottom-blur";
 
-export default function Home() {
+export default async function Home({
+  params,
+}: {
+  params: Promise<{ slug: string }>
+}) {
+  const { slug } = await params
   return (
     <div className={styles.page}>
       <div className={styles['text-container']}>
         <p className={styles.title}>Olivia</p>
         <p className={styles['sub-title']}>faz 1</p>
       </div>
-      <TransparentLinkButton href={'/album'}>Venha me ver crescer</TransparentLinkButton>
+      <TransparentLinkButton href={`${slug}/album`}>Venha me ver crescer</TransparentLinkButton>
       <div className={styles['cover-image']}>
         <Image
           className={styles['bg-image']}
